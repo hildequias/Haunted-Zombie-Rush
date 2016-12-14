4d4680b06de34b6f1518069cbec7ff1f7bf85616
Hildequias Silas dos Santos Junior <br3hss@ecenmbrgp.lojas.brasil.latam.cea>
2016-12-14 15:01:00 -0200

    added player animation, physics and colliders

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButtonDown (0)) {
+			
+		}
+	}
+}
