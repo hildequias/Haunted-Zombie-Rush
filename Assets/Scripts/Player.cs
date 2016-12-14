9d6d0f954a451128aa0e294abd1bebd8a3c8b912
Hildequias Silas dos Santos Junior <br3hss@ecenmbrgp.brasil.latam.cea>
2016-12-14 15:57:45 -0200

    add force to jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,35 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	Animator anim;
+	[SerializeField] private float jumpForce = 100f;
+	private Animator anim;
+	private Rigidbody rigidBody;
+	private bool jump = false;
 
 	// Use this for initialization
 	void Start () {
 
 		anim = GetComponent<Animator> ();
+		rigidBody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		if (Input.GetMouseButtonDown (0)) {
-			
+
+			anim.Play ("Jump");
+			rigidBody.useGravity = true;
+			jump = true;
+		}
+	}
+
+	void FixedUpdate() {
+
+		if(jump == true) {
+			jump = false;
+			rigidBody.velocity = new Vector2 (0, 0);
+			rigidBody.AddForce (new Vector2 (0,jumpForce), ForceMode.Impulse);
 		}
 	}
 }
